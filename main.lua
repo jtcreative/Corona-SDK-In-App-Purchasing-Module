af1953355f2253cbda9c5946f725683eaf1bdc49
James <jamestimberlakejr@gmail.com>
2012-07-21 00:00:56 -0400

    added commentation on main.lua

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 local ui = require("ui")
 local storeMod = require("storeMod")
 
+----@start interface features here we are making some interface features to get the storemod working
 local screen = display.newGroup()
 
 local background = display.newRect(0,0,display.contentWidth, display.contentHeight)
@@ -10,6 +11,7 @@ background:setFillColor(255,255,0)
 
 screen:insert(background)
 
+---we can use this to restore the store connection
  local button = ui.newButton({
    default= "button.png",
    over="button.png",
@@ -23,6 +25,13 @@ screen:insert(background)
  button:translate(display.contentWidth*0.8,display.contentHeight*0.8)
  screen:insert(button)
 
+---@end interface features
+
+
+--- below is all the code you need to activate the storeMod
+---this function sets the transaction event callback for making purchases in the app
+----NOTE, if you need to download your in-app purchases, you may need to alter the transaction event function in storeMod
+---to work like that
 storeMod.setTransactionEvent(function(state, transaction)
   if state == 'purchased' then
     print("success buying item "..transaction.productIdentifier)
@@ -37,6 +46,8 @@ local products = {
 	"com.anscamobile.NewExampleInAppPurchase.NonConsumableTier1",
 }
 
+---this gives you all the information you need form your products
+---pressing this activates the store and gives you the product information
 storeMod.startStore(products,function(validProducts, invalidProducts)
      --now you can poplate your info to make in app purchasing
   for k,v in ipairs(validProducts) do
